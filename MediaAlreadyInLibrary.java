555ec4b69a1dcc89c7d1414765a2a301ff3ec0cc
Alessandro Gaiarin <agaiarin@gmail.com>
2021-11-09 14:26:09 -0500

    MediaAlreadyInLibrary exception is complete. Untested, but I'm pretty sure it'll work.

---
+++ MediaAlreadyInLibrary.java
@@ -0,0 +1,26 @@
+/**
+ * Exception. For when two or more labeled media items with the same
+ * ISBN are added to the media library.
+ * @version 0.1
+ * @author Alessandro Gaiarin
+ */
+
+//TODO:
+// Fairly sure this will just work.
+
+public class MediaAlreadyInLibrary extends Exception{
+    private String name;
+    private Media media; //reference to duplicate media item
+
+    //Getters:
+    public String getName() {return name;}
+    public Media getMedia() {return media;}
+
+    //Constructor:
+    public MediaAlreadyInLibrary(String name, Media media) {
+        super("Media already in libary.");
+        this.name = name;
+        this.media = media;
+    }
+
+}
